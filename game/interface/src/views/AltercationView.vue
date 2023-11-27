684de543535b48068b8e705fb8afa9784e74cba6
mekhinini <elies.mekhinini@etu.unistra.fr>
2023-11-27 12:15:09 +0100

    :sparkles: animation de text (a revoir)

--- game/interface/src/views/AltercationView.vue
+++ game/interface/src/views/AltercationView.vue
@@ -50,7 +50,30 @@ export default {
       position: absolute;
       border: black 1px solid;
       inset-inline: 100px;
+      max-height: 200px;
+      overflow: hidden;
     }
+      .text p {
+        overflow: hidden; /* Ensures the content is not revealed until the animation */
+        border-right: .15em solid orange; /* The typwriter cursor */
+        //white-space: nowrap; /* Keeps the content on a single line */
+        margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+        //letter-spacing: .15em; /* Adjust as needed */
+        animation:
+            typing 3.5s steps(40, end),
+            blink-caret .75s step-end infinite;
+      }
+      /* The typing effect */
+      @keyframes typing {
+        from { width: 0 }
+        to { width: 100% }
+      }
+
+      /* The typewriter cursor effect */
+      @keyframes blink-caret {
+        from, to { border-color: transparent }
+        50% { border-color: orange; }
+      }
 
     .interaction {
       position: absolute;
