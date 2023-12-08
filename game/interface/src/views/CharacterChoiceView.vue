df154a8f445ed82286ae4a59a8c84ef6e4dab800
mekhinini <elies.mekhinini@etu.unistra.fr>
2023-12-08 15:15:56 +0100

    :poop: squelette de CSS character choice

--- game/interface/src/views/CharacterChoiceView.vue
+++ game/interface/src/views/CharacterChoiceView.vue
@@ -1,14 +1,30 @@
 <script>
 export default {
-  name: "CharacterChoiceView"
+  name: "CharacterChoiceView",
+  
+  data() {
+    return {
+      player: {
+        
+      }
+    }
+  }
 }
 </script>
 
 <template>
   <div class="ecran">
-    <div class="sexeChoice"></div>
+    <div class="sexeChoice">
+      <a href="" class="leftSexeBtn"><img src="../../images/buttons/left_semi_arrow.png" alt="left_semi_arrow"></a>
+      <p>sexe : choix</p>
+      <a href="" class="rightSexeBtn"><img src="../../images/buttons/right_semi_arrow.png" alt="right_semi_arrow"></a>
+    </div>
     <div class="skinChoice"></div>
-    <div class="outfitChoice"></div>
+    <div class="outfitChoice">
+      <a href="" class="leftOutfitBtn"><img src="../../images/buttons/left_semi_arrow.png" alt="left_semi_arrow"></a>
+      <img src="" alt="player" class="showPlayer">
+      <a href="" class="rightOutfitBtn"><img src="../../images/buttons/right_semi_arrow.png" alt="right_semi_arrow"></a>
+    </div>
     <div class="navigation"></div>
   </div>
 </template>
@@ -24,6 +40,10 @@ export default {
 }
   .sexeChoice {
     position: absolute;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
     width: 100%;
     height: 100px;
     top: 0;
@@ -31,6 +51,17 @@ export default {
 
     background-color: red;
   }
+
+    .leftSexeBtn{
+      //position: absolute;
+      width: 60px;
+      height: 60px;
+    }
+    .rightSexeBtn{
+      //position: absolute;
+      width: 60px;
+      height: 60px;
+    }
 
   .skinChoice {
     position: absolute;
@@ -44,13 +75,35 @@ export default {
 
   .outfitChoice {
     position: absolute;
-    width: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 375px;
     height: 450px;
     top: 100px;
     right: 300px;
 
     background-color: green;
   }
+    .leftOutfitBtn{
+      //position: absolute;
+      width: 60px;
+      height: 60px;
+    }
+    .rightOutfitBtn{
+      //position: absolute;
+      width: 60px;
+      height: 60px;
+    }
+    .showPlayer {
+      position: absolute;
+      width: 150px;
+      height: 350px;
+      top: 10px;
+      background-color: brown;
+    }
 
   .navigation {
     position: absolute;
