485289d664a5c285b56c541399b9bf5b89685756
loicmaes <97891718+loicmaes@users.noreply.github.com>
2023-12-17 14:52:27 +0100

    :ambulance: Fix conflicts

--- game/api/handlers/files.handler.mjs
+++ game/api/handlers/files.handler.mjs
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:game/api/handlers/files.handler.mjs
 /**
  * Search all js files in a specified directory.
  * @author Loïc MAES
@@ -23,29 +22,3 @@ export default function recover (path, recursive) {
 
     return files
 }
-========
-/**
- * Search all js files in a specified directory.
- * @author Loïc MAES
- * @copy All rights reserves © 2023
- */
-const fs = require('fs')
-
-function isDirectory (path) {
-    return fs.lstatSync(path).isDirectory()
-}
-
-function recover (path, recursive) {
-    const files = []
-
-    fs.readdirSync(path).forEach(file => {
-        const child = `${path}/${file}`
-        if (recursive && isDirectory(child)) recover(`${child}/`, recursive).forEach(f => files.push(`${file}/${f}`))
-        else if (file.endsWith('.js')) files.push(file)
-    })
-
-    return files
-}
-
-module.exports = recover
->>>>>>>> origin/main:game/api/handlers/fileHandler.js
