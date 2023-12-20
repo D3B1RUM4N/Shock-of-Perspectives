67e517cf9aea49f583961116c9146696edd20f42
Eléa Menu <emenu@unistra.fr>
2023-12-20 12:43:16 +0100

    :art: Add CharacterChoiceView CSS

--- game/interface/src/views/CharacterChoiceView.vue
+++ game/interface/src/views/CharacterChoiceView.vue
@@ -45,11 +45,11 @@ export default {
   <div class="ecran">
     <div class="sexeChoice">
       <a href="" class="leftSexeBtn" @click.prevent="nextSexe"><img src="/images/buttons/left_semi_arrow.png" alt="left_semi_arrow"></a>
-      <p>sexe : {{ stringSexe }}</p>
+      <p>Sexe : {{ stringSexe }}</p>
       <a href="" class="rightSexeBtn" @click.prevent="nextSexe"><img src="/images/buttons/right_semi_arrow.png" alt="right_semi_arrow"></a>
     </div>
     <div class="skinChoice">
-      <p>skin : {{ stringColor }}</p>
+      <p>Skin : {{ stringColor }}</p>
       <div class="whiteSkin" @click.prevent="nextColor(2)"></div>
       <div class="yellowSkin" @click.prevent="nextColor(1)"></div>
       <div class="blackSkin" @click.prevent="nextColor(0)"></div>
@@ -75,8 +75,9 @@ export default {
   display:flex;
   justify-content: center;
   align-items: center;
+  background-color: #BBD3F0;
 
-  background-color: grey;
+  font-family: "Press Start 2P", Serif;
 }
 
   .sexeChoice {
@@ -89,37 +90,37 @@ export default {
     height: 130px;
     top: 0;
     left: 0;
+    font-size:30px;
 
-    background-color: red;
+    background-color: #79A1D0;
   }
 
     .leftSexeBtn{
-      //position: absolute;
+      position: absolute;
+      left: 270px;
       width: 60px;
       height: 60px;
     }
     .rightSexeBtn{
-      //position: absolute;
+      position: absolute;
+      right:270px;
       width: 60px;
       height: 60px;
     }
 
   .skinChoice {
     position: absolute;
-    width: 300px;
+    width: 320px;
     height: 460px;
     top: 130px;
     left: 0;
 
+    padding-left:20px;
     display: flex;
     flex-direction: column;
-    //justify-content: center;
-    align-items: center;
+    align-items: flex-start;
     //space items inside the div
     justify-content: space-around;
-
-
-    background-color: blue;
   }
 
     .whiteSkin {
@@ -129,10 +130,18 @@ export default {
 
       width: 94px;
       height: 94px;
+      margin-left : 40px;
 
       background: #FAD5BA;
       border: 4px solid #000000;
       border-radius: 50%;
+
+      transition: transform 0.5s ease, box-shadow 0.3s ease;
+    }
+
+    .whiteSkin:hover{
+      box-shadow: 0 0 15px 5px white;
+      transform: translateY(-5px);
     }
 
     .blackSkin {
@@ -142,12 +151,20 @@ export default {
 
       width: 94px;
       height: 94px;
+      margin-left : 40px;
 
       background: #5D342F;
       border: 4px solid #000000;
 
       border-radius: 50%;
+
+      transition: transform 0.5s ease, box-shadow 0.3s ease;
     }
+
+  .blackSkin:hover{
+    box-shadow: 0 0 15px 5px white;
+    transform: translateY(-5px);
+  }
 
     .yellowSkin {
       /* Ellipse 3 */
@@ -156,13 +173,20 @@ export default {
 
       width: 94px;
       height: 94px;
+      margin-left : 40px;
 
       background: #FCCE7F;
       border: 4px solid #000000;
 
 
       border-radius: 50%;
+
+      transition: transform 0.5s ease, box-shadow 0.3s ease;
     }
+  .yellowSkin:hover{
+    box-shadow: 0 0 15px 5px white;
+    transform: translateY(-5px);
+  }
 
   .outfitChoice {
     display: flex;
@@ -175,21 +199,22 @@ export default {
     top: 130px;
     right: 300px;
 
-    background-color: green;
   }
     .leftOutfitBtn{
-      width: 60px;
-      height: 60px;
+      width: 80px;
+      height: 80px;
     }
+
     .rightOutfitBtn{
-      width: 60px;
-      height: 60px;
+      width: 80px;
+      height: 80px;
     }
     .showPlayer {
-      background-color: red;
-      width: 150px;
+      width: 200px;
       height: 350px;
       overflow: hidden;
+      object-fit: cover;
+
     }
 
   .navigation {
@@ -205,22 +230,26 @@ export default {
     bottom: 0;
     left: 0;
 
-    background-color: yellow;
   }
 
     .previous {
-      width: 60px;
-      height: 60px;
+      width: 100px;
+      height: 100px;
+      position: absolute;
+      right:88%;
     }
 
     .navigation p {
       width: 200px;
       height: 60px;
       font-size: 30px;
+      margin-bottom: 150px;
     }
 
     .next {
-      width: 60px;
-      height: 60px;
+      width: 100px;
+      height: 100px;
+      position: absolute;
+      left : 88%;
     }
 </style>
