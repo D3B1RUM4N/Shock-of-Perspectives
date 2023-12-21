056c1fe9e5cdfe357ceb7f75205fabfcacc62f7e
loicmaes <production@maesloic.fr>
2023-12-21 14:33:35 +0100

    :rocket: Deploy data models

---
+++ game/interface/src/assets/js/models/altercation.model.js
@@ -0,0 +1,39 @@
+import store from '@/store'
+import { selectRandom } from '@/assets/js/utils/list.utils'
+
+export class Altercation {
+    id
+    label
+    dojo
+    character
+    
+    constructor (payload) {
+        const { id, label, dojo } = payload
+        
+        this.id        = id
+        this.label     = label
+        this.dojo      = dojo
+        this.character = selectRandom(GameController.CHARACTERS)
+    }
+    
+    react (reaction) {
+        store.dispatch('reactTo', {
+            altercation: this.id,
+            reaction: reaction.id
+        })
+    }
+}
+
+export class Reaction {
+    id
+    label
+    icon
+    
+    constructor (payload) {
+        const { id, label, icon } = payload
+        
+        this.id    = id
+        this.label = label
+        this.icon  = icon
+    }
+}
