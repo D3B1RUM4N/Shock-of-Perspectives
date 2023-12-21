4f245b6594ea0f06e95df118523c672fbfb3e895
loicmaes <production@maesloic.fr>
2023-12-21 14:34:13 +0100

    :rocket: Bind controller and actions to store

--- game/interface/src/store/index.js
+++ game/interface/src/store/index.js
@@ -1,20 +1,115 @@
+import axios from 'axios'
+import router from '@/router'
 import { createStore } from 'vuex'
-import Character from "../../public/Model/Character";
+import { randomString } from '@/assets/js/utils/strings.utils'
+import { GameController, Resume } from '@/assets/js/controllers/game.controller'
+import { Character, CharacterOptions } from '@/assets/js/models/character.model'
 
 export default createStore({
   state: {
-    Controller: null
+    controller: new GameController(),
+    resume: undefined
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
-    // set Controller
-    setController(state, controller) {
-      state.Controller = controller;
+    storeCharacters (state, payload) {
+      state.controller.storeCharacters(payload.map(c => {
+        const { gender, skin, csp, name } = c
+        const options = new CharacterOptions(gender, skin, csp)
+        return new Character(name, options)
+      }))
     },
+    storeCriteria (state, payload) {
+      const { genders, skins, csps } = payload
+
+      state.controller.storeGenders(genders)
+      state.controller.storeSkins(skins)
+      state.controller.storeCsps(csps)
+    },
+    storeReactions (state, payload) {
+      state.controller.storeReactions(payload.map(({id, label, dojo}) => new Reaction(id, label, dojo)))
+    },
+    setNewAltercation (state, payload) {
+      state.controller.setAltercation(payload)
+    },
+    storeSession (state, payload) {
+      state.controller.storeSession(payload)
+    },
+    storeResume (state, payload) {
+      const { stats, history } = payload
+      state.resume = new Resume(
+          stats,
+          history
+      )
+    }
   },
   actions: {
+    async askCriteria (state) {
+      try {
+        const { data, status } = await axios.get('/characters/criteria')
+        if (status === 204) return console.error('No criteria found!')
+        state.commit('storeCriteria', data)
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    async askCharacters (state) {
+      try {
+        const { data, status } = await axios.get('/characters')
+        if (status === 204) return console.error('No characters found!')
+        state.commit('storeCharacters', data)
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    async askReactions (state) {
+      try {
+        const { data, status } = await axios.get('/reactions')
+        if (status === 204) return console.error('No reactions found!')
+        state.commit('storeCharacters', data)
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    async nextAltercation (state) {
+      await router.push('/loading')
+      try {
+        const { data } = await axios.get('/altercations')
+        state.commit('setNewAltercation', data)
+        setTimeout(() => router.push('/altercation'), 1000)
+      } catch (e) {
+        console.error(e)
+        await router.push('/')
+      }
+    },
+    async createSession (state, options) {
+      try {
+        const { data } = await axios.post('/session', {
+          name: randomString(Math.floor(Math.random() * 15) + 1),
+          ...options
+        })
+        state.commit('storeSession', data)
+        state.dispatch('nextAltercation')
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    async askResume (state, sessionCode) {
+      const { code } = state.controller.session
+      try {
+        const resSessionStats = await axios.get(`/session/${code}`)
+        const resGameHistory  = await axios.get(`/game/resume/${code}`)
+        if (resSessionStats.status === 204) return console.error('No stats found for the current session!')
+        if (resGameHistory.status === 204) return console.error('No history found for the current session!')
+
+        state.commit('storeResume', {
+          stats: resSessionStats.data,
+          history: resGameHistory.data
+        })
+      } catch (e) {
+        console.error(e)
+      }
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
